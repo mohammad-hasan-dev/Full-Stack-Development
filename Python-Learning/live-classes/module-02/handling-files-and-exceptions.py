da30205aa932af072ae752cf8df70dddb81d3660
Mohammad Hasan <mohammadhasan.dev@gmail.com>
2026-09-25 01:39:52 +0600

    Add Python live class Module 02 and Module 03

---
+++ Python-Learning/live-classes/module-02/handling-files-and-exceptions.py
@@ -0,0 +1,71 @@
+# print(5 / 0)
+# if True
+# print("Hi")
+
+# A = 5
+# print(a)
+
+# print(5 + 5)
+# print("A" + 5)
+
+# a = int("A")
+# print(a)
+
+# arr = ["abc", "xyz"]
+# print(arr[5])
+
+# d = {"name": "Zayed"}
+# print(d["rolll"])
+
+
+# try:
+#     a = int(input("Enter a number: "))
+#     b = int(input("Enter a number to divide: "))
+#     print("Result is:", a / b)
+# except ValueError:
+#     print("Eiiii...........")
+# except ZeroDivisionError:
+#     print("Boss, 0 diye to vag kora jay na!")
+# except Exception as e:
+#     print("Kichu ekta hoise!")
+#     print(e)
+
+
+#
+# Files
+#
+
+# f = open("about.txt")
+
+# data = f.read()
+# print(data)
+
+# f.seek(5)
+# print(f.read())
+# f.seek(0)
+# print(f.read())
+
+# f.close()
+
+# print(data)
+
+# f = open("about2.txt", "a")
+
+# # print(f.read())
+# f.write("\nThis is a big villian!\n")
+
+# f.close()
+# f = open("habijabi.txt")
+
+# f = open("E:\\code\\hobby\\ostad\\claudecodeb02m03p2\\agent_sdk_hello.py")
+
+# "code/hobby"
+
+# print(f.read())
+
+# f.close()
+
+# f = open("about/../../pythondjangob13m02/about/../about/me.txt")
+# f = open("E:\\code\\hobby\\ostad\\pythondjangob13m02\\about\\me.txt")
+
+# f.close()
---
+++ Python-Learning/live-classes/module-03/python-oop-essentials.py
@@ -0,0 +1,5 @@
+class student:
+    pass
+a= student()
+
+print (a)
